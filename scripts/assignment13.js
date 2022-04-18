c4e287f52673276ff1a64709cf5fe48b1274b7f2
Devon Quispe <devon.quispe@colorado.edu>
2022-04-18 13:52:19 -0600

    assignment 13

--- scripts/assignment13.js
+++ scripts/assignment13.js
@@ -12,21 +12,42 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-randomize.addEventListener('click', result);
+// randomize.addEventListener('click', result);
 
 function result() {
+  console.log("clicked")
+  var newStory = storyText;
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
 
   if(customName.value !== '')
   {
     const name = customName.value;
+    newStory = newStory.replace('Bob', name);
   }
 
   if(document.getElementById("uk").checked)
   {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300 / 14).toString() + ' stone';
+    const temperature =  Math.round((94 - 32) * 5/9).toString() + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
-  story.textContent = '';
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
+
+
+function windowLoaded() {
+  console.log('loaded');
+  document.getElementById('r_button').addEventListener('click', result)
+}
+
+window.onload = windowLoaded
+
