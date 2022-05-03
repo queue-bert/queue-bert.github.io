5eed168845cdd575b5f35aac6cc54869a0b86bc6
Devon Quispe <devon.quispe@colorado.edu>
2022-05-02 21:53:54 -0600

    finished basic sound board

--- music.js
+++ music.js
@@ -11,31 +11,44 @@
 //   }
 // });
 
-var keyElement = document.getElementById("play");
+var container = document.getElementById('container')
+children = container.childNodes
+for(var i = 0; i < children.length; i++)
+{
+    console.log(children[i].id)
+    children[i].addEventListener("touchstart", notePressed, false);
+    children[i].addEventListener("touchend", noteReleased, false);
+    children[i].addEventListener("mousedown", notePressed, false); 
+    children[i].addEventListener("mouseup", noteReleased, false);
+}
 
-keyElement.addEventListener("touchstart", notePressed, false);
-keyElement.addEventListener("touchend", noteReleased, false);
+// var keyElement = document.getElementById("play");
+
+// keyElement.addEventListener("touchstart", notePressed, false);
+// keyElement.addEventListener("touchend", noteReleased, false);
 
 // Added mouse ELs to test on non-mobile.
-keyElement.addEventListener("mousedown", notePressed, false); 
-keyElement.addEventListener("mouseup", noteReleased, false);
+// keyElement.addEventListener("mousedown", notePressed, false); 
+// keyElement.addEventListener("mouseup", noteReleased, false);
 
 var pressedTone;
 
 function notePressed(event) {
-  console.log("notePressed");
-  pressedTone = playTone(440);
-  event.preventDefault();
-  event.stopPropagation();
+    console.log(event.target.innerHTML)
+    freq = event.target.innerHTML
+    console.log("notePressed");
+    pressedTone = playTone(freq);
+    event.preventDefault();
+    event.stopPropagation();
 }
 function noteReleased(event) {
-  pressedTone.triggerRelease();
+    pressedTone.triggerRelease();
 }
 function playTone(freq) {
-  console.log("playTone", freq);
-  let synth = new Tone.Synth().toDestination();
-  let now = Tone.now();
-  Tone.start();
-  synth.triggerAttack(freq, now);
-  return synth;
+    console.log("playTone", freq);
+    let synth = new Tone.Synth().toDestination();
+    let now = Tone.now();
+    Tone.start();
+    synth.triggerAttack(freq, now);
+    return synth;
 }
